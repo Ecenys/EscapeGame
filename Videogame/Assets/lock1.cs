bab485f4bbe1638ce9a95ddd09c2e345f760148e
JOSE ANTONIO SERRANO ESPARCIA <dwravensfantasy@gmail.com>
2019-04-27 12:22:13 +0200

    key 1
    chest, me queda arreglar el movimiento.
    arreglados tamaños de la mesa e impresora, la llave 1 es la que deja abrir el cofre

---
+++ Videogame/Assets/lock1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lock1 : MonoBehaviour
+{
+    public GameObject box;
+    public GameObject colision;
+    
+    
+    public int a=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        a = 3;
+        if (col.gameObject.tag == "keyG")
+        {
+            box.GetComponent<BoxCollider>().enabled = true;
+            a = 2;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
