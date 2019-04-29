26660af9cd5d1af58254d8f37c1353da8a1ccaf8
JOSE ANTONIO SERRANO ESPARCIA <dwravensfantasy@gmail.com>
2019-04-29 10:12:52 +0200

    caja 1 + wind
    asdf

---
+++ Videogame/Assets/pang.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pang : MonoBehaviour
+{
+    public GameObject box;
+    public GameObject colision;
+
+
+    public int a = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        a = 3;
+        if (col.gameObject.tag == "pang")
+        {
+
+            Destroy(box);
+            a = 2;
+        }
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}
---
+++ Videogame/Assets/passwordA.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class passwordA : MonoBehaviour
+{
+
+    public GameObject box;
+    public int a = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    void OpenA()
+    {
+
+        box.GetComponent<BoxCollider>().enabled = true;
+        a = 2;
+        
+    }
+}
