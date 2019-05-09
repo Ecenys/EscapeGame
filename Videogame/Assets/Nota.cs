93f74f3b3682c50f363feac9a1360e1c9c5b3cff
JorgeCamachoTorres <ctjorge97@gmail.com>
2019-05-09 21:57:55 +0200

    Added functional piano

---
+++ Videogame/Assets/Nota.cs
@@ -0,0 +1,30 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class Nota : MonoBehaviour
+    {
+        public AudioSource source;
+        public AudioClip nota;
+        void Start()
+        {
+            source = GetComponent<AudioSource>();
+        }
+
+        public void OnButtonDown(Hand fromHand)
+        {
+            source.PlayOneShot(nota, 0.5f);
+        }
+
+        public void OnButtonUp(Hand fromHand)
+        {
+
+        }
+
+    }
+}
--- Videogame/Assets/Scripts/Light2script.cs
+++ Videogame/Assets/Scripts/Light2script.cs
@@ -9,7 +9,7 @@ public class Light2script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Light>().enabled = false;
+        GetComponent<Light>().enabled = true;
     }
 
     // Update is called once per frame
