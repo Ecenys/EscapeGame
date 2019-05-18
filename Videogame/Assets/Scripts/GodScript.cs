dfa15214d37adea264d8601ccdae47d54a159130
Oscar Gomez <elcelestial97@outlook.com>
2019-05-18 13:46:49 +0200

    Varios
    Sistema de pistas implementadas, arreglo de pared

--- Videogame/Assets/Scripts/GodScript.cs
+++ Videogame/Assets/Scripts/GodScript.cs
@@ -6,57 +6,91 @@ using Valve.VR;
 
 public class GodScript : MonoBehaviour
 {
+	// Audios
+	public AudioClip audio1;
+    public AudioClip audio2;
+    public AudioClip audio3;
+    public AudioClip audio4;
+	
+	//SteamVR actions
     public SteamVR_Action_Boolean grabPinch;
     public SteamVR_Input_Sources handType;
 
-	private float time;
-	private int hint; 
-	
+	//Global parameters
+    private float time;
+    private int hint;
+    private AudioSource audioData;
+    private bool press; //evita que el audio se inicie cada frame si lo mantiene pulsado
+    
     // Start is called before the first frame update
     void Start()
     {
+        //audio = GetComponent<AudioSource>();
         time = 0;
+        hint = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
-        if (time < 5) ;
+        if (time < 1) ; //Evitar que despues del inicio se ejecute de manera innecesaria
             quitaGuante();
-        if (grabPinch.GetStateDown(handType))
-            getHint();
+        if (grabPinch.GetStateDown(handType) && !press){
+            sayHint();
+            press = true;
+        }
+        else
+            press = false;
     }
-	
-	public float getTime(){
-		return time;
-	}
-	
-	public void getHint(){
-		switch(hint){
-			case 1:
-				Debug.Log("1");
-				break;
-			case 2:
-				Debug.Log("2");
-				break;
-			case 3:
-				Debug.Log("3");
-				break;
-			case 4:
-				Debug.Log("4");
-				break;
-		}
-	}
-
+    
+    //************************** Time **************************//
+    public float getTime(){
+        return time;
+    }
+    
+    
+    //************************** Music **************************//
+    //Decir pista
+    public void sayHint(){
+        GetComponent<AudioSource>().Stop();
+        //yield return new WaitForSeconds(audio.clip.length);
+        GetComponent<AudioSource>().Play();
+    }
+    
+    //Cambiar pista
+    private void changeHint(){
+        switch(hint){
+            case 1:
+                GetComponent<AudioSource>().clip = audio1;
+                Debug.Log("1");
+                break;
+            case 2:
+                GetComponent<AudioSource>().clip = audio2;
+                Debug.Log("2");
+                break;
+            case 3:
+                GetComponent<AudioSource>().clip = audio3;
+                Debug.Log("3");
+                break;
+            case 4:
+                GetComponent<AudioSource>().clip = audio4;
+                Debug.Log("4");
+                break;
+        }
+    }
+    
     public void nextHint() {
         hint++;
+        GetComponent<AudioSource>().Stop();
+        changeHint();
     }
 
     public void specialAudio(int i) {
             
     }
 
+    //************************** Quitar mando **************************//
     void quitaGuante() {
         for (int handIndex = 0; handIndex < Player.instance.hands.Length; handIndex++)
         {
@@ -76,3 +110,5 @@ public class GodScript : MonoBehaviour
         }
     }
 }
+
+
