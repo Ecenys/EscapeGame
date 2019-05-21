924b9c2e095ee420348f1bc0d6c78af2eb13f8ca
Javier Gualda <javiercagn@gmail.com>
2019-05-21 13:48:36 +0200

    Added final audio

---
+++ Videogame/Assets/FinalAudio.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalAudio : MonoBehaviour
+{
+
+    public GameObject a;
+    private AudioSource cosa;
+    private bool counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cosa = a.GetComponent<AudioSource>();
+        counter = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!counter)
+        {
+            //Cerrar la puerta de atras?
+            cosa.Play();
+            counter = true;
+        }
+    }
+}
