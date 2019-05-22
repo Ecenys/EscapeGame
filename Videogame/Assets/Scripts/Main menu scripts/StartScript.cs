6aa1b6bf3f7f140a9f0eabd5e196985b5528d070
JorgeCamachoTorres <ctjorge97@gmail.com>
2019-05-22 19:35:18 +0200

    Trying to fix main menu

--- Videogame/Assets/Scripts/Main menu scripts/StartScript.cs
+++ Videogame/Assets/Scripts/Main menu scripts/StartScript.cs
@@ -6,6 +6,7 @@ public class StartScript : MonoBehaviour
 {
     public GameObject controller1;
 	public GameObject controller2;
+    public GameObject player;
 	
     // Update is called once per frame
     void Update()
@@ -13,7 +14,15 @@ public class StartScript : MonoBehaviour
         if (Vector3.Distance(controller1.transform.position, transform.position) < 1 || Vector3.Distance(controller2.transform.position, transform.position) < 1 )
 			//acciones extra (movimiento npc)
 			Application.LoadLevel("Amazing videogame");
-		if (Input.GetKeyDown( KeyCode.Space ))
+        if (Vector3.Distance(player.transform.position, transform.position) < 0.1f || Vector3.Distance(player.transform.position, transform.position) < 0.1f)
+            Application.LoadLevel("Amazing videogame");
+            //acciones extra (movimiento npc)
+        if (Input.GetKeyDown( KeyCode.Space ))
 			Application.LoadLevel("Amazing videogame");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Application.LoadLevel("Amazing videogame");
     }
 }
