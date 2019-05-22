07b0c8e5bb3b7675e1249bc2962573f856330418
Javier Gualda <javiercagn@gmail.com>
2019-05-22 19:46:31 +0200

    TPController

---
+++ Videogame/Assets/InitTP.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitTP : MonoBehaviour
+{
+    public GameObject tpor;
+    public GameObject tpentrada;
+    public GameObject tpmusica;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tpor.SetActive(true);
+        tpentrada.SetActive(true);
+        tpmusica.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
