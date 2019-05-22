6cf5e7d75a73d1164b6fe1b6618f1ba7024b2821
JorgeCamachoTorres <ctjorge97@gmail.com>
2019-05-22 19:12:32 +0200

    Fixed general bugs

---
+++ Videogame/Assets/GritoMujer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GritoMujer : MonoBehaviour
+{
+    public AudioSource source;
+    public bool var=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!var)
+        {
+            source.Play();
+            var = true;
+        }
+    }
+}
