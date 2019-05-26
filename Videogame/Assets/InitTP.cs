36dcc2038c59375d19bb958831451c3021285a23
JorgeCamachoTorres <ctjorge97@gmail.com>
2019-05-26 19:22:25 +0200

    All tps fixed

--- Videogame/Assets/InitTP.cs
+++ Videogame/Assets/InitTP.cs
@@ -8,13 +8,15 @@ public class InitTP : MonoBehaviour
     public GameObject tpentrada;
     public GameObject tpmusica;
     public GameObject tpotro;
+    public GameObject tpJa;
     // Start is called before the first frame update
     void Start()
     {
         tpor.SetActive(true);
         tpentrada.SetActive(true);
-        //tpmusica.SetActive(true);
+        tpmusica.SetActive(true);
         tpotro.SetActive(true);
+        tpJa.SetActive(true);
     }
 
     // Update is called once per frame
